e4c9d02e2d3802e58fa57880b5d29d40facca04c
Ben Grimes <bgrimes@salesforce.com>
2014-03-02 16:53:01 -0600

    Add logout button

--- tasks/cat_feeder.js
+++ tasks/cat_feeder.js
@@ -66,7 +66,7 @@ module.exports = function(grunt) {
 
     // watch
     grunt.config.set('watch.catFeeder', {
-        files: '<%= catFeeder.files %>',
+        files: base + '{,*}*',
         tasks: ['catFeeder']
     });
 
